39ba454ae8abc4b67e9e0d6af81d23be5d2e6d1b
kirak98 <kirak98@hotmail.co.uk>
2023-09-27 11:13:44 +0200

    dictionary files created, search engine set up

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Dictionary from './Dictionary';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Dictionary</h1>
-       
+       <Dictionary/>
       </header>
+      <footer> Open Source coded by Kira King </footer>
     </div>
   );
 }
