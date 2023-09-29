ab949b69301016ef64d0115d9cf3100f7f00e8b8
kirak98 <kirak98@hotmail.co.uk>
2023-09-29 10:38:54 +0200

    api and axios import

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,22 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    
+    function handleResposnse(response) {
+        console.log(response);
+
+    }
+
     function search(event) {
         event.preventDefault();
+        
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={keyword}&key={apiKey}`;
+        apiKey = "f1cc8436d5e8084ob3affafba54dtceb";
+        axios.get(apirUrl).then(handleResposnse);
     }
     function handleKeywordChange(event) {
         console.log(keyword)
