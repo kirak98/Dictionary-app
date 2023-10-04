6d9e0f5f5746a09ae2fd3047e72cbebf703b5962
kirak98 <kirak98@hotmail.co.uk>
2023-10-04 12:26:55 +0200

    stying and sections added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,9 +26,12 @@ export default function Dictionary() {
     }
     return (
         <div className="Dictionary">
+            <section>
             <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
+                <input type="search" placeholder="Search for a word..." onChange={handleKeywordChange} />
+                
             </form>
+            </section>
             <Results results={results} />
         </div>
     )
