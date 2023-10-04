4fb7437b4222605e1dda356584124b55ecd7e9fe
kirak98 <kirak98@hotmail.co.uk>
2023-10-04 19:51:07 +0200

    image api, photos and styling added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Photos from "./Photos";
 import Results from "./Results";
 
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState (null);
+    const [keyword, setKeyword] = useState("");
+const [results, setResults] = useState (null);
+    const [photos, setPhotos] = useState ([]);
     
+    function handleImages(response) {
+        setPhotos(response.data.photos);
+      }
+
     function handleResponse(response) {
         setResults(response.data);
+        let apiKey = "f1cc8436d5e8084ob3affafba54dtceb";
+        let apiUrl = `https://api.shecodes.io/images/v1/search?query=${response.data.word}&key=${apiKey}`;
+        axios.get(apiUrl, {headers: {Authorization: `Bearer ${apiKey}`}}).then(handleImages);
 
     }
 
@@ -33,6 +42,7 @@ export default function Dictionary() {
             </form>
             </section>
             <Results results={results} />
+            <Photos photos={photos} />
         </div>
     )
 }
